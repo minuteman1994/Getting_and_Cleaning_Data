578e1b3de543752075b40867972fc689fb9ab891
JasonTurner <jasonturner72@gmail.com>
2015-02-22 11:19:19 -0800

    first commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,8 @@
-# 1) a tidy data set as described below, 
-# 2) a link to a Github repository with your script for performing the analysis, and 
-# 3) a code book that describes the variables, the data, and any transformations 
-#   or work that you performed to clean up the data called CodeBook.md. 
-# You should also include a README.md in the repo with your scripts. 
-# This repo explains how all of the scripts work and how they are connected.  
-
-# You should create one R script called run_analysis.R that does the following. 
-
-#You should create one R script called run_analysis.R that does the following. 
-
+# 1. A tidy data set as described below, 
+# 2. A link to a Github repository with your script for performing the analysis, and 
+# 3. A code book that describes the variables, the data, and any transformations or work that you performed to clean up the data called CodeBook.md. 
+# 4. You should also include a README.md in the repo with your scripts. This repo explains how all of the scripts work and how they are connected.  
+# 5. You should create one R script called run_analysis.R that does the following.
 
 setwd("C:/Users/root/Documents/rprogramming/datacleaning")
 library(plyr)
@@ -21,9 +15,7 @@ features[,2] = gsub('[-()]', '', features[,2])
 features[,2] = gsub(',', '', features[,2])
 features[,2] = tolower(features[,2])
 
-
 # Objective 1: Merges the training and the test sets to create one data set.
-# the meaty data:
 xtrain <- read.table("UCI HAR Dataset/train/X_train.txt")
 # the activity labels of the obs, so these need to get into the data in a column
 ytrain <- read.table("UCI HAR Dataset/train/y_train.txt")
@@ -33,8 +25,7 @@ colnames(subjecttrain) = c("subject")
 colnames(xtrain) = features$V2
 train <- cbind(subjecttrain,ytrain,xtrain)
 
-
-# and again for the test files:
+# the test files:
 xtest  <- read.table("UCI HAR Dataset/test/X_test.txt")
 ytest  <- read.table("UCI HAR Dataset/test/y_test.txt")
 subjecttest <- read.table("UCI HAR Dataset/test//subject_test.txt")
